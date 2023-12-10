4b1a7164f09ad1099e1cd3b409ef004480dcab5f
brian-ambani <brainambani1@gmail.com>
2023-12-10 12:12:33 +0300

    updated the routes in the App

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/nav/Nav";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Workouts from "./pages/Workouts";
+import Nopage from "./pages/Nopage";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/workouts" element={<Workouts />} />
+          <Route path="*" element={<Nopage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
